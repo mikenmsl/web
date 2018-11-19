f0ff0b30ce2a979bc00f45a0bde07114a63950c1
mikenmsl <mikenmsl@gmail.com>
2018-11-19 22:28:14 +0300

    Update main.js

--- vueProject/IPS/src/main.js
+++ vueProject/IPS/src/main.js
@@ -1,20 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-
-
 import AppMainTableScreen from "./components/MainTableScreen.vue";
 import AppSourceTableScreen from "./components/SourceTableScreen.vue";
 import AppStartScreen from "./components/StartScreen.vue";
 import AppViewDate from "./components/ViewDate.vue";
 
-
 Vue.component('AppMainTableScreen', AppMainTableScreen);
 Vue.component('AppSourceTableScreen', AppSourceTableScreen);
 Vue.component('AppStartScreen', AppStartScreen);
 Vue.component('AppViewDate', AppViewDate);
-
-
 
 new Vue({
   el: '#app',
